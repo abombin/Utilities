1ad2e00cd8f4cba00f11d2f1dd3a84d05b9b58ad
abombin <abombin93@gmail.com>
2022-09-19 15:36:16 +0000

    first version to download the latest run from S3 bucket

--- downlodProjS3.py
+++ downlodProjS3.py
@@ -1,26 +1,59 @@
 import boto3
 import os
+import argparse
 
+
+parser=argparse.ArgumentParser(description='Transfer latest run from S3 bucket')
+
+# add arguments
+
+parser.add_argument(
+  "-p",
+  "--project",
+  type=str,
+  nargs="?",
+  default="virus",
+  help="project name, virus or ICMC"
+)
+
+# parse arguments
+args=parser.parse_args()
+
+project=args.project
+
+#S3
 s3 = boto3.resource('s3')
 
+# read from S3
 obj = s3.Object('transfer-files-emory', 'download.me')
 file=obj.get()['Body'].read().decode('utf-8')
 
+# split S3 path to components
 file_split=file.split('/')
-
 
 project_name=file_split[3]
 
 run_nmae=file_split[4]
 
-print(project_name, run_nmae)
+# select output directory where to put files
+def project_out_dir(project):
+    if project=='virus':
+        project_path='C:/Users/Administrator/OneDrive - Emory University/virus/output/'
+    elif project == 'ICMC':
+        project_path='C:/Users/Administrator/OneDrive - Emory University/ICMC'
+    return(project_path)
 
-run_path='C:\\Users\\Administrator\\OneDrive - Emory University\\virus\\output\\%s\\%s' % \
-    (project_name, run_nmae)
+run_path=project_out_dir(project=project)
 
-print(run_path)
+# create directory and download
+def downloadDirectoryFroms3(bucketName, remoteDirectoryName):
+    s3_resource = boto3.resource('s3')
+    bucket = s3_resource.Bucket(bucketName) 
+    for obj in bucket.objects.filter(Prefix = remoteDirectoryName):
+        out_path=(run_path+obj.key)
+        if not os.path.exists(os.path.dirname(out_path)):
+            os.makedirs(os.path.dirname(out_path))
+        bucket.download_file(obj.key, out_path)
 
-if not os.path.exists(run_path):
-    os.makedirs(run_path)
-else:
-    print('path exists')
+
+downloadDirectoryFroms3(bucketName='transfer-files-emory', remoteDirectoryName=project_name)
